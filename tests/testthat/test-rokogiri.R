9dd4b4a1aea1cad4ec7e80ce7824dfde42d489e3
RobertZK <technoguyrob@gmail.com>
2015-02-20 01:37:25 -0600

    passing list output test

--- tests/testthat/test-rokogiri.R
+++ tests/testthat/test-rokogiri.R
@@ -1,12 +1,28 @@
 context('rokogiri')
 
+test_that('it can produce a simple pre-XML list output', {
+  xml <- rokogiri(output_type = 'list', {
+    note({
+      to("Tove")
+      from("Jani")
+      heading("Reminder")
+      msg("Don't forget me this weekend!")
+    })
+  })
+ 
+  expected_list <- list(note = list(to = "Tove",
+    from = "Jani", heading = "Reminder", msg = "Don't forget me this weekend!"))
+
+  expect_identical(xml, expected_list)
+})
+
 test_that('it can produce a simple example XML', {
   xml <- rokogiri({
     note({
       to("Tove")
       from("Jani")
       heading("Reminder")
-      body("Don't forget me this weekend!")
+      msg("Don't forget me this weekend!")
     })
   })
  
@@ -15,10 +31,11 @@ test_that('it can produce a simple example XML', {
       <to>Tove</to>
       <from>Jani</from>
       <heading>Reminder</heading>
-      <body>Don't forget me this weekend!</body>
+      <msg>Don't forget me this weekend!</msg>
     </note>
   "
   
   remove_spacing <- function(x) { gsub("[ \n]", "", x) }
   expect_equal(remove_spacing(xml), remove_spacing(expected_xml))
 })
+
